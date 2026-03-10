88fb42851fb28f01139e05077e83ea84e891beec
shilpa1819AS <spaipati@gmail.com>
2026-03-10 17:55:18 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=>{
-  res.send("DevOps CI/CD Project Running")
+  res.send("Hello from CI/CD v2");
 })
 
 app.listen(3000)
